ad296bb34832e16dac8ac295c4e6d1e03de088e7
shwetaj1 <shewatakumari1@gmail.com>
2024-11-04 00:57:12 +0530

    added currency exchange app

---
+++ src/main/java/com/aj/gitlabciexample/GitlabciexampleApplication.java
@@ -0,0 +1,13 @@
+package com.aj.gitlabciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabciexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabciexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aj/gitlabciexample/GitlabciexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.gitlabciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabciexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
